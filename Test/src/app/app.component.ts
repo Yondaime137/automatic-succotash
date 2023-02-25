73f1de21af4e1e4910afd323e43757e2daec31e6
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-02-25 23:12:59 +0100

    adding Karabas

--- Test/src/app/app.component.ts
+++ Test/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test';
+
+  users: any  = [];
+
+  karabas = (input: string) => {
+    this.users.push(input);
+  }
 }
